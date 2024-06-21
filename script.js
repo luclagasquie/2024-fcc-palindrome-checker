8d3373237501042d27988e3668e21e49260848e6
Luc Lagasquie <sensodia@gmail.com>
2024-06-21 15:22:34 +0200

    Finalize script

--- script.js
+++ script.js
@@ -2,13 +2,28 @@ const checkBtn = document.getElementById('check-btn');
 const textInput = document.getElementById('text-input');
 const result = document.getElementById('result');
 
+function cleanInputString(str) {
+    const regex = /[\W_]/g;
+    return str.replace(regex, '');
+  }
+
 const getInput = () => {
     const inputValue = textInput.value;
-    if (inputValue.length === 0) {
+    const lowerCaseValue = inputValue.toLowerCase();
+    const cleanValue = cleanInputString(lowerCaseValue).split("");
+
+    const reverseValue = cleanValue.slice().reverse();
+
+    if (cleanValue.length === 0) {
         alert("Please input a value");
+    } else if (cleanValue.join() === reverseValue.join()) {
+        result.innerText = `${inputValue} is a palindrome`;
     } else {
-        checkBtn.innerText = "vrai";
+        result.innerText = `${inputValue} is not a palindrome`;
     }
+    
+    console.log(cleanValue);
+    console.log(reverseValue);
 }
 
 checkBtn.addEventListener("click", getInput);
